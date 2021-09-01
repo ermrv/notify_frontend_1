73bd4cc65a606d0ee44eda597caabd1241b01d30
ermrv <eermrv@gmail.com>
2021-09-01 11:26:55 +0530

    minor

---
+++ lib/MODULES/15_NotificationModule/views/NotificationPageScreen.dart
@@ -0,0 +1,11 @@
+import 'package:MediaPlus/MODULES/15_NotificationModule/controllers/NotificationPageController.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NotificationPageScreen extends StatelessWidget {
+  final controller = Get.put(NotificationPageController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
