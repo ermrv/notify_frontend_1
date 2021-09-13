c25a9d9284b51d42b34c2da04093bb73b82e073e
ermrv <eermrv@gmail.com>
2021-09-13 12:36:01 +0530

    minor

--- lib/MODULES/17_ShortVideoPlayerModule/views/ShortVideoPlayerPageScreen.dart
+++ lib/MODULES/17_ShortVideoPlayerModule/views/ShortVideoPlayerPageScreen.dart
@@ -29,7 +29,7 @@ class _ShortVideoPlayerPageScreenState
 
   @override
   void initState() {
-    if (widget.postContent !=null) {
+    if (widget.postContent != null) {
       videos.add(widget.postContent);
     }
     _getData();
