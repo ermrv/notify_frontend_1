d78b58e8bb2ee1c23bee2e09af9d0ce813c2686a
ermrv <eermrv@gmail.com>
2021-10-13 11:26:59 +0530

    user drawer modified

--- lib/MODULES/6_HomePageModule/UserDrawerModule/views/UserDrawerScreen.dart
+++ lib/MODULES/6_HomePageModule/UserDrawerModule/views/UserDrawerScreen.dart
@@ -19,190 +19,145 @@ import 'package:get_storage/get_storage.dart';
 class UserDrawerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: screenWidth * 0.6,
-      decoration: BoxDecoration(
-          color: Theme.of(context).appBarTheme.backgroundColor,
-          border: Border(right: BorderSide())),
-      child: Scaffold(
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            title: Row(
-              children: [
-                Container(
-                  height: 50.0,
-                  width: 50.0,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                          color: Theme.of(context).accentColor, width: 1.0)),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50.0),
-                      child: Obx(
-                        () => CachedNetworkImage(
-                          imageUrl:
-                              PrimaryUserData.primaryUserData.profilePic.value,
-                          fit: BoxFit.cover,
-                        ),
-                      )),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 10.0),
-                  child: Obx(
-                    () => Text(
-                      PrimaryUserData.primaryUserData.name.value.toString(),
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18.0),
-                    ),
-                  ),
-                )
-              ],
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Get.back();
+              },
             ),
-          ),
-          body: Column(
+            Container(
+              width: 10.0,
+            )
+          ],
+          automaticallyImplyLeading: false,
+          title: Row(
             children: [
               Container(
-                // decoration: BoxDecoration(border: Border(bottom: BorderSide())),
-                margin: EdgeInsets.only(left: 8.0, right: 18.0),
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: TextButton(
+                height: 50.0,
+                width: 50.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        color: Theme.of(context).accentColor, width: 1.0)),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Obx(
+                      () => CachedNetworkImage(
+                        imageUrl:
+                            PrimaryUserData.primaryUserData.profilePic.value,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10.0),
+                child: Obx(
+                  () => Text(
+                    PrimaryUserData.primaryUserData.name.value.toString(),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              // decoration: BoxDecoration(border: Border(bottom: BorderSide())),
+              margin: EdgeInsets.only(left: 8.0, right: 18.0),
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: TextButton(
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.resolveWith((states) =>
+                        EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 10.0))),
+                onPressed: () {
+                  Get.to(() => UserProfileScreen(
+                        profileOwnerId: PrimaryUserData.primaryUserData.userId,
+                      ));
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Full Profile",
+                    ),
+                    Text(">>>")
+                  ],
+                ),
+              ),
+            ),
+            // Container(
+            //   height: 150.0,
+            //   child: Column(
+            //     children: [
+            //       Container(
+            //         alignment: Alignment.centerLeft,
+            //         margin:
+            //             EdgeInsets.symmetric(horizontal: 3.0, vertical: 5.0),
+            //         child: Text("Contest:"),
+            //       ),
+            //       Container(
+            //           margin: EdgeInsets.symmetric(horizontal: 18.0),
+            //           padding: EdgeInsets.symmetric(vertical: 5.0),
+            //           child: TextButton(
+            //               style: ButtonStyle(
+            //                   padding: MaterialStateProperty.resolveWith(
+            //                       (states) => EdgeInsets.symmetric(
+            //                           horizontal: 10.0, vertical: 10.0))),
+            //               onPressed: () {
+            //                 Get.to(() => ContestParticipationHistoryScreen());
+            //               },
+            //               child: Row(
+            //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //                 children: [
+            //                   Text("Contest Participation"),
+            //                   Text(">>>")
+            //                 ],
+            //               ))),
+            //       Container(
+            //           margin: EdgeInsets.symmetric(horizontal: 18.0),
+            //           padding: EdgeInsets.symmetric(vertical: 5.0),
+            //           child: TextButton(
+            //               style: ButtonStyle(
+            //                   padding: MaterialStateProperty.resolveWith(
+            //                       (states) => EdgeInsets.symmetric(
+            //                           horizontal: 10.0, vertical: 10.0))),
+            //               onPressed: () {
+            //                 Get.to(() => ContestHostingHistoryScreen());
+            //               },
+            //               child: Row(
+            //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //                 children: [Text("Contest Hosting"), Text(">>>")],
+            //               ))),
+            //     ],
+            //   ),
+            // ),
+            Expanded(child: Container()),
+            Container(
+              margin: EdgeInsets.only(left: 8.0, right: 18.0),
+              padding: EdgeInsets.symmetric(vertical: 5.0),
+              child: TextButton(
                   style: ButtonStyle(
                       padding: MaterialStateProperty.resolveWith((states) =>
                           EdgeInsets.symmetric(
                               horizontal: 10.0, vertical: 10.0))),
                   onPressed: () {
-                    Get.to(() => UserProfileScreen(
-                          profileOwnerId:
-                              PrimaryUserData.primaryUserData.userId,
-                        ));
+                    ThemeController().changeTheme("light");
                   },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        "Full Profile",
-                      ),
-                      Text(">>>")
+                      Text("Change Theme"),
                     ],
-                  ),
-                ),
-              ),
-              // Container(
-              //   height: 150.0,
-              //   child: Column(
-              //     children: [
-              //       Container(
-              //         alignment: Alignment.centerLeft,
-              //         margin:
-              //             EdgeInsets.symmetric(horizontal: 3.0, vertical: 5.0),
-              //         child: Text("Contest:"),
-              //       ),
-              //       Container(
-              //           margin: EdgeInsets.symmetric(horizontal: 18.0),
-              //           padding: EdgeInsets.symmetric(vertical: 5.0),
-              //           child: TextButton(
-              //               style: ButtonStyle(
-              //                   padding: MaterialStateProperty.resolveWith(
-              //                       (states) => EdgeInsets.symmetric(
-              //                           horizontal: 10.0, vertical: 10.0))),
-              //               onPressed: () {
-              //                 Get.to(() => ContestParticipationHistoryScreen());
-              //               },
-              //               child: Row(
-              //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //                 children: [
-              //                   Text("Contest Participation"),
-              //                   Text(">>>")
-              //                 ],
-              //               ))),
-              //       Container(
-              //           margin: EdgeInsets.symmetric(horizontal: 18.0),
-              //           padding: EdgeInsets.symmetric(vertical: 5.0),
-              //           child: TextButton(
-              //               style: ButtonStyle(
-              //                   padding: MaterialStateProperty.resolveWith(
-              //                       (states) => EdgeInsets.symmetric(
-              //                           horizontal: 10.0, vertical: 10.0))),
-              //               onPressed: () {
-              //                 Get.to(() => ContestHostingHistoryScreen());
-              //               },
-              //               child: Row(
-              //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //                 children: [Text("Contest Hosting"), Text(">>>")],
-              //               ))),
-              //     ],
-              //   ),
-              // ),
-              Expanded(child: Container()),
-              Container(
-                margin: EdgeInsets.only(left: 8.0, right: 18.0),
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                child: TextButton(
-                    style: ButtonStyle(
-                        padding: MaterialStateProperty.resolveWith((states) =>
-                            EdgeInsets.symmetric(
-                                horizontal: 10.0, vertical: 10.0))),
-                    onPressed: () {
-                      ThemeController().changeTheme("light");
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Change Theme"),
-                      ],
-                    )),
-              ),
-              Container(
-                  margin: EdgeInsets.only(left: 8.0, right: 18.0),
-                  padding: EdgeInsets.symmetric(vertical: 5.0),
-                  child: TextButton(
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.resolveWith((states) =>
-                              EdgeInsets.symmetric(
-                                  horizontal: 10.0, vertical: 10.0))),
-                      onPressed: () async {
-                        var response = await ApiServices.postWithAuth(
-                            ApiUrlsData.domain + "/api/notification",
-                            {},
-                            userToken);
-                        print(response);
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [Text("Get notifications"), Text(">>>")],
-                      ))),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                height: 1.0,
-                color: Theme.of(context).accentColor.withOpacity(0.3),
-              ),
-              Container(
-                  margin: EdgeInsets.only(left: 8.0, right: 18.0),
-                  padding: EdgeInsets.symmetric(vertical: 5.0),
-                  child: TextButton(
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.resolveWith((states) =>
-                              EdgeInsets.symmetric(
-                                  horizontal: 10.0, vertical: 10.0))),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [Text("Settings"), Text(">>>")],
-                      ))),
-              // Container(
-              //     margin: EdgeInsets.symmetric(horizontal: 8.0),
-              //     padding: EdgeInsets.symmetric(vertical: 5.0),
-              //     child: TextButton(
-              //         style: ButtonStyle(
-              //             padding: MaterialStateProperty.resolveWith((states) =>
-              //                 EdgeInsets.symmetric(
-              //                     horizontal: 10.0, vertical: 10.0))),
-              //         onPressed: () {},
-              //         child: Row(
-              //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //           children: [Text("Change Theme"), Text(">>>")],
-              //         ))),
-              Container(
+                  )),
+            ),
+            Container(
                 margin: EdgeInsets.only(left: 8.0, right: 18.0),
                 padding: EdgeInsets.symmetric(vertical: 5.0),
                 child: TextButton(
@@ -211,32 +166,78 @@ class UserDrawerScreen extends StatelessWidget {
                             EdgeInsets.symmetric(
                                 horizontal: 10.0, vertical: 10.0))),
                     onPressed: () async {
-                      final storage = GetStorage();
-                      userToken = "";
-                      await _deleteFileSystem().then((value) async {
-                        if (value) {
-                          await storage.remove("unRegisteredUserToken").then(
-                              (value) async => await storage
-                                  .remove("userToken")
-                                  .then((value) =>
-                                      Get.offAll(() => LoginScreen())));
-                        } else {
-                          Get.snackbar(
-                              "Cannot process request", "Please try again");
-                        }
-                      });
+                      var response = await ApiServices.postWithAuth(
+                          ApiUrlsData.domain + "/api/notification",
+                          {},
+                          userToken);
+                      print(response);
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("LogOut"),
-                        Icon(Icons.power_settings_new)
-                      ],
-                    )),
-              ),
-            ],
-          )),
-    );
+                      children: [Text("Get notifications"), Text(">>>")],
+                    ))),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+              height: 1.0,
+              color: Theme.of(context).accentColor.withOpacity(0.3),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 8.0, right: 18.0),
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                child: TextButton(
+                    style: ButtonStyle(
+                        padding: MaterialStateProperty.resolveWith((states) =>
+                            EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 10.0))),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("Settings"), Text(">>>")],
+                    ))),
+            // Container(
+            //     margin: EdgeInsets.symmetric(horizontal: 8.0),
+            //     padding: EdgeInsets.symmetric(vertical: 5.0),
+            //     child: TextButton(
+            //         style: ButtonStyle(
+            //             padding: MaterialStateProperty.resolveWith((states) =>
+            //                 EdgeInsets.symmetric(
+            //                     horizontal: 10.0, vertical: 10.0))),
+            //         onPressed: () {},
+            //         child: Row(
+            //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //           children: [Text("Change Theme"), Text(">>>")],
+            //         ))),
+            Container(
+              margin: EdgeInsets.only(left: 8.0, right: 18.0),
+              padding: EdgeInsets.symmetric(vertical: 5.0),
+              child: TextButton(
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.resolveWith((states) =>
+                          EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 10.0))),
+                  onPressed: () async {
+                    final storage = GetStorage();
+                    userToken = "";
+                    await _deleteFileSystem().then((value) async {
+                      if (value) {
+                        await storage.remove("unRegisteredUserToken").then(
+                            (value) async => await storage
+                                .remove("userToken")
+                                .then((value) =>
+                                    Get.offAll(() => LoginScreen())));
+                      } else {
+                        Get.snackbar(
+                            "Cannot process request", "Please try again");
+                      }
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text("LogOut"), Icon(Icons.power_settings_new)],
+                  )),
+            ),
+          ],
+        ));
   }
 
   Future<bool> _deleteFileSystem() async {
--- lib/MODULES/6_HomePageModule/views/HomePageScreen.dart
+++ lib/MODULES/6_HomePageModule/views/HomePageScreen.dart
@@ -5,7 +5,6 @@ import 'package:MediaPlus/MODULES/14_MainNavigationModule/controllers/MainNaviga
 import 'package:MediaPlus/SERVICES_AND_UTILS/KeepWidgetAliveModule/KeepWidgetAliveWrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'package:foldable_sidebar/foldable_sidebar.dart';
 import 'package:get/get.dart';
 
 //widget with [AutomaticKeepAliveClientMixin] having the tabbar view with
@@ -19,7 +18,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   AnimationController _animationController;
-  FSBStatus fsbStatus = FSBStatus.FSB_CLOSE;
   // Animation<double> _animation;
   // GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
   final mainNavigationController = Get.find<MainNavigationController>();
@@ -44,105 +42,35 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return FoldableSidebarBuilder(
-      status: fsbStatus,
-      drawer: UserDrawerScreen(),
-      screenContents: Scaffold(
-        // onDrawerChanged: (isOpened) {
-        //   if (isOpened) {
-        //     mainNavigationController.hide();
-        //   }
-        //   if (!isOpened) {
-        //     mainNavigationController.show();
-        //   }
-        // },
-        // key: _drawerKey,
-        // drawer: Drawer(
-        //   child: UserDrawerScreen(),
-        // ),
-        appBar: AppBar(
-          centerTitle: true,
-          leading: GestureDetector(
-            onTap: () {
-              // _drawerKey.currentState.openDrawer();
-              if (fsbStatus == FSBStatus.FSB_CLOSE) {
-                fsbStatus = FSBStatus.FSB_OPEN;
-                mainNavigationController.hide();
-                setState(() {});
-              } else {
-                if (fsbStatus == FSBStatus.FSB_OPEN) {
-                  fsbStatus = FSBStatus.FSB_CLOSE;
-                  mainNavigationController.show();
-                  setState(() {});
-                }
-              }
-            },
-            // child: Stack(
-            //   children: [
-            //     Container(
-            //       height: 35.0,
-            //       width: 35.0,
-            //       decoration: BoxDecoration(
-            //           shape: BoxShape.circle,
-            //           color: Colors.grey),
-            //       child: ClipRRect(
-            //         borderRadius:
-            //             BorderRadius.circular(30.0),
-            //         child: PrimaryUserData.primaryUserData
-            //                     .profilePic ==
-            //                 null
-            //             ? Image.asset("assets/person.jpg",
-            //                 fit: BoxFit.fill)
-            //             : CachedNetworkImage(
-            //                 imageUrl: PrimaryUserData
-            //                     .primaryUserData.profilePic
-            //                     .toString(),
-            //                 fit: BoxFit.fill,
-            //               ),
-            //       ),
-            //     ),
-            //     Positioned(
-            //         bottom: 1.0,
-            //         right: 0.0,
-            //         child: Container(
-            //           padding: EdgeInsets.all(1.0),
-            //           // height: 15.0,
-            //           // width: 15.0,
-            //           decoration: BoxDecoration(
-            //               color: Colors.white,
-            //               shape: BoxShape.circle),
-            //           child: Icon(
-            //             Icons.menu,
-            //             color: Colors.blue,
-            //             size: 11.0,
-            //           ),
-            //         ))
-            //   ],
-            child: fsbStatus == FSBStatus.FSB_CLOSE
-                ? Icon(Entypo.grid)
-                : Icon(Icons.close),
-          ),
-          title: Text(
-            "mediaPlus",
-            style: TextStyle(
-                fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Feather.search,
-                  size: 22.0,
-                ),
-                onPressed: () {
-                  Get.to(() => SearchInputPageScreen());
-                }),
-            Container(
-              width: 3.0,
-            )
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Entypo.grid),
+          onPressed: () {
+            Get.to(() => UserDrawerScreen(),transition: Transition.leftToRightWithFade);
+          },
         ),
-        body: NewsFeedPageScreen(),
+        title: Text(
+          "mediaPlus",
+          style: TextStyle(
+              fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Feather.search,
+                size: 22.0,
+              ),
+              onPressed: () {
+                Get.to(() => SearchInputPageScreen());
+              }),
+          Container(
+            width: 3.0,
+          )
+        ],
       ),
+      body: NewsFeedPageScreen(),
     );
   }
 
